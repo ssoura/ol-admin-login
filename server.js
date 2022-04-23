05f0c068ac8fd79e9512a6ec9468bdf395864915
Sourabh Shukla <sourabhshukla13689@gmail.com>
2022-04-23 19:08:15 +0530

    Create controller functions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 
@@ -11,16 +12,18 @@ var corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.json())
 
-const db = require("./models");
+const db = require("./app/models")
 // db.sequelize.sync()
 
 // Drop and re-sync db for development
-db.sequelize.sync({ force: true });
+db.sequelize.sync({ force: true })
 
 app.get("/", (req, res) => {
     res.json({ message: 'Hi, Welcome to application.'})
 })
 
+// api routes
+require("./app/routes/customer.routes")(app)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
