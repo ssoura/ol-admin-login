4b2ad235015bfd8e139e46effc34c3157819da19
Sourabh Shukla <sourabhshukla13689@gmail.com>
2022-04-26 16:55:15 +0530

    update

--- app/middleware/verifySignUp.js
+++ app/middleware/verifySignUp.js
@@ -1,57 +1,57 @@
-const db = require("../models");
-const ROLES = db.ROLES;
-const User = db.user;
+const db = require("../models")
+const ROLES = db.ROLES
+const User = db.user
 
-checkDuplicateUsernameOrEmail = (req, res, next) => {
+checkDuplicateUsernameOrPhone = (req, res, next) => {
   // Username
   User.findOne({
     where: {
-      username: req.body.username
-    }
-  }).then(user => {
+      username: req.body.username,
+    },
+  }).then((user) => {
     if (user) {
       res.status(400).send({
-        message: "Failed! Username is already in use!"
-      });
-      return;
+        message: "Failed! Username is already in use!",
+      })
+      return
     }
 
-    // Email
+    // Phone
     User.findOne({
       where: {
-        email: req.body.email
-      }
-    }).then(user => {
+        email: req.body.phone,
+      },
+    }).then((user) => {
       if (user) {
         res.status(400).send({
-          message: "Failed! Email is already in use!"
-        });
-        return;
+          message: "Failed! Phone is already in use!",
+        })
+        return
       }
 
-      next();
-    });
-  });
-};
+      next()
+    })
+  })
+}
 
 checkRolesExisted = (req, res, next) => {
   if (req.body.roles) {
     for (let i = 0; i < req.body.roles.length; i++) {
       if (!ROLES.includes(req.body.roles[i])) {
         res.status(400).send({
-          message: "Failed! Role does not exist = " + req.body.roles[i]
-        });
-        return;
+          message: "Failed! Role does not exist = " + req.body.roles[i],
+        })
+        return
       }
     }
   }
-  
-  next();
-};
+
+  next()
+}
 
 const verifySignUp = {
-  checkDuplicateUsernameOrEmail: checkDuplicateUsernameOrEmail,
-  checkRolesExisted: checkRolesExisted
-};
+  checkDuplicateUsernameOrPhone: checkDuplicateUsernameOrPhone,
+  checkRolesExisted: checkRolesExisted,
+}
 
-module.exports = verifySignUp;
+module.exports = verifySignUp
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,15 +1,15 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("users", {
     username: {
-      type: Sequelize.STRING
+      type: Sequelize.STRING,
     },
-    email: {
-      type: Sequelize.STRING
+    phone: {
+      type: Sequelize.INTEGER,
     },
     password: {
-      type: Sequelize.STRING
-    }
-  });
+      type: Sequelize.STRING,
+    },
+  })
 
-  return User;
-};
+  return User
+}
